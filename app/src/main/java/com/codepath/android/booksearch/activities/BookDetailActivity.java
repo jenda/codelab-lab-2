d64cef5866ecb2ec33fa8df81b34871f8ff7fcb2
Jan Spidlen <jan.spidlen@airbnb.com>
2017-09-20 20:25:20 -0700

    -shared toolbar - toolbar displays title

--- app/src/main/java/com/codepath/android/booksearch/activities/BookDetailActivity.java
+++ app/src/main/java/com/codepath/android/booksearch/activities/BookDetailActivity.java
@@ -2,7 +2,10 @@ package com.codepath.android.booksearch.activities;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,6 +13,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.GlideBitmapDrawable;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.codepath.android.booksearch.R;
 import com.codepath.android.booksearch.models.Book;
 import com.codepath.android.booksearch.utils.Constants;
@@ -21,6 +28,8 @@ public class BookDetailActivity extends AppCompatActivity {
     private TextView tvTitle;
     private TextView tvAuthor;
     private Book book;
+    private ShareActionProvider miShareAction;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,16 +42,37 @@ public class BookDetailActivity extends AppCompatActivity {
 
         // Extract book object from intent extras
         book = (Book) getIntent().getParcelableExtra(Constants.BOOK_DATA);
-//        Log.d("jenda", "book: " + book.getTitle());
-        // Use book object to populate data into views
 
         tvTitle.setText(book.getTitle());
         tvAuthor.setText(book.getAuthor());
 
         Glide.with(this)
                 .load(Uri.parse(book.getCoverUrl()))
+                .listener(new RequestListener<Uri, GlideDrawable>() {
+                      @Override
+                      public boolean onException(Exception e, Uri model, Target<GlideDrawable> target, boolean isFirstResource) {
+                          return false;
+                      }
+
+                      @Override
+                      public boolean onResourceReady(GlideDrawable resource, Uri model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+//                                  prepareShareIntent(((GlideBitmapDrawable) resource).getBitmap());
+//                                  attachShareIntentAction();
+                          // Let Glide handle resource load
+                          return false;
+                      }
+                  }
+                )
                 .placeholder(R.drawable.ic_nocover)
                 .into(ivBookCover);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle(book.getTitle());
+
+        // get publish_year here
     }
 
 
@@ -50,6 +80,11 @@ public class BookDetailActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_book_detail, menu);
+
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        // Fetch reference to the share action provider
+        miShareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
         return true;
     }
 
